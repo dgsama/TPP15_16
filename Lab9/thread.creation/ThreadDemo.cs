fffcaceaa7b3b190f575c5ad83232cada59a2503
unknown <uo237464@EII-L-11-02.aulas.uniovi.es>
2016-04-14 18:51:21 +0200

    cambio en ThreadDemo

--- Lab9/thread.creation/ThreadDemo.cs
+++ Lab9/thread.creation/ThreadDemo.cs
@@ -2,18 +2,26 @@
 using System.Threading;
 using System.Text;
 
-namespace TPP.Laboratory.Concurrency.Lab09 {
+namespace TPP.Laboratory.Concurrency.Lab09
+{
 
-    class ThreadDemo {
+    class ThreadDemo
+    {
 
-        static void Main(string[] args) {
-            if (args.Length > 0) {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
                 int numberOfThreads = Convert.ToInt32(args[0]);
                 MessageThread[] obj = new MessageThread[numberOfThreads];
-                for (int i = 0; i < numberOfThreads; i++) 
-                    new Thread(new MessageThread().Run).Start();
+                for (int i = 0; i < numberOfThreads; i++)
+                {
+                    var t = new Thread(new MessageThread().Run)/*.Start()*/;
+                    t.IsBackground = true;
+                    t.Start();
+                }
             }
-            new MessageThread().Run();
+            //new MessageThread().Run();
         }
 
     }
