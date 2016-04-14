a735f25efcc83e1ee10b8c130dda41bc998ca558
unknown <uo237464@EII-L-11-02.aulas.uniovi.es>
2016-04-14 18:17:07 +0200

    Lab 9 inicio

---
+++ Lab9/context.switching/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace TPP.Laboratory.Concurrency.Lab09 {
+
+    class Program {
+
+        static void Main(string[] args) {
+            const int maxNumberOfThreads = 50;
+            short[] vector = VectorModulusProgram.CreateRandomVector(100000, -10, 10);
+            ShowLine(Console.Out, "Number of threads", "Ticks", "Result");
+            for (int numberOfThreads = 1; numberOfThreads <= maxNumberOfThreads; numberOfThreads++) {
+                Master master = new Master(vector, numberOfThreads);
+                DateTime before = DateTime.Now;
+                double result = master.ComputeModulus();
+                DateTime after = DateTime.Now;
+                ShowLine(Console.Out, numberOfThreads, (after - before).Ticks, result);
+                GC.Collect(); // Garbage collection
+                GC.WaitForFullGCComplete();
+            }
+        }
+
+        private const string CSV_SEPARATOR = ";";
+
+        static void ShowLine(TextWriter stream, string numberOfThreadsTitle, string ticksTitle, string resultTitle) {
+            stream.WriteLine("{0}{3}{1}{3}{2}{3}", numberOfThreadsTitle, ticksTitle, resultTitle, CSV_SEPARATOR);
+        }
+
+        static void ShowLine(TextWriter stream, int numberOfThreads, long ticks, double result) {
+            stream.WriteLine("{0}{3}{1:N0}{3}{2:N2}{3}", numberOfThreads, ticks, result, CSV_SEPARATOR);
+        }
+    }
+
+}
---
+++ Lab9/process.thread/ProcessThreadDemo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TPP.Laboratory.Concurrency.Lab09 {
+
+    class ProcessThreadDemo {
+
+        private void ShowProcesses(Process[] process, TextWriter output) {
+            foreach (Process proc in process) {
+                output.WriteLine("- PID: {0}\tName: {1}\tVirtual memory: {2:N} MB",
+                        proc.Id, proc.ProcessName, proc.VirtualMemorySize64 / 1024.0 / 1024
+                        );
+            }
+        }
+
+        private void ShowProcess(Process process, TextWriter output, bool showMemory) {
+            output.Write("- PID: {0}\tName: {1}", process.Id, process.ProcessName);
+            if (showMemory)
+                output.Write("\tVirtual memory: {2:N} MB",process.VirtualMemorySize64 / 1024.0 / 1024);
+            output.WriteLine(".");
+        }
+
+        private void ShowThreads(ProcessThreadCollection collection, TextWriter output) {
+            foreach (ProcessThread thread in collection) 
+                output.WriteLine("\t- ThreadId: {0}\tPriority: {1}\tState: {2}.",
+                    thread.Id, thread.CurrentPriority, thread.ThreadState);
+        }
+
+        static void Main() {
+            ProcessThreadDemo demo = new ProcessThreadDemo();
+            var processes = Process.GetProcesses();
+            demo.ShowProcesses(processes, Console.Out);
+            
+            Console.Write("Press enter to continue... ");
+            Console.ReadLine();
+
+            foreach (var process in processes) {
+                demo.ShowProcess(process, Console.Out, false);
+                demo.ShowThreads(process.Threads, Console.Out);
+            }
+        }
+
+    }
+}
